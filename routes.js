65d870794d6bb4a03257cdfb7e1216e7bc92dd8f
bartekringwelski <bartekr@gmail.com>
2017-01-24 17:14:23 -0800

    keys change for heroku

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
-const keys = require('./keys');
+// const keys = require('./keys');
+
+const keys = process.env.DATA_GOV_KEY || require('./keys');
 const apiConversion = require('./apiConversion');
 
 
