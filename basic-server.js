62d3e2d43a8f265ed66528ec3cc1e4409cb325d5
bartekringwelski <bartekr@gmail.com>
2017-01-26 14:05:09 -0800

    deleted un neede files

--- basic-server.js
+++ basic-server.js
@@ -3,19 +3,15 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser')
 const router = require('./routes');
-// var morgan = require('morgan');
-
-// app.use(morgan('combined'));
+// var morgan = require('morgan'); app.use(morgan('combined'));
 
 var PORT = process.env.PORT || 3000;
 
-
 app.use(bodyParser.json());
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.post('/userSubmission', router.submitToCensus)
 
 app.listen(PORT);
 
-console.log('listening on port ' + PORT );
+console.log('listening on port ' + PORT);
